0eed074a422d8e661e3bc49097de93e04b4011e7
Pasquale Boemio <pasquale.boemio@statwolf.com>
2017-01-11 17:52:40 +0100

    0.0.2

--- dist/ingect.js
+++ dist/ingect.js
@@ -1,5 +1,5 @@
 /*
-  ingect v0.0.1
+  ingect v0.0.2
   
 
   @author:  Pasquale Boemio <boemianrapsodi@gmail.com>
@@ -25,7 +25,17 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 
       if (store[moduleName].resolved === undefined) {
         store[moduleName].deps.forEach(function(depName) {
-          deps.push(resolve(store, depName));
+          if (_typeof(store[depName]) === 'object') {
+            deps.push(resolve(store, depName));
+            return;
+          }
+
+          if (window[depName] !== undefined) {
+            deps.push(window[depName]);
+            return;
+          }
+
+          throw new Error(depName + ' is an invalid dependency for module ' + moduleName);
         });
 
         store[moduleName].resolved = store[moduleName].module.apply(null, deps);
